44e3dd7250b2b85e826f8cf6d6c71e8cee179985
Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>
2023-01-05 13:50:36 -0500

    addressing pr feedback

--- src/plugins/base64_encode_header.rs
+++ src/plugins/base64_encode_header.rs
@@ -29,9 +29,12 @@ impl Plugin for Base64EncodeHeader {
         ServiceBuilder::new()
             .map_request(move |mut req: subgraph::Request| {
                 if let Some(header) = req.supergraph_request.headers().get("authorization") {
-                    req.subgraph_request
-                        .headers_mut()
-                        .append("Authorization64", base64::encode(header).parse().unwrap());
+                    req.subgraph_request.headers_mut().append(
+                        "Authorization64",
+                        base64::encode(header)
+                            .parse()
+                            .expect("Failed to base64 encode"),
+                    );
                 }
                 req
             })
